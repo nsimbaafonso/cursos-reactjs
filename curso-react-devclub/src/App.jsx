c7462d3e6f62cabfc09026e0930ef03af377288e
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-08-23 14:38:50 +0100

    Novo layout

--- curso-react-devclub/src/App.jsx
+++ curso-react-devclub/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  function CliqueiNoBotao(params) {
+    alert("Botão clicado");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className=" flex items-center justify-center bg-gray-100 p-6">
+      <div className="w-full max-w-md bg-white shadow-xl rounded-2xl p-6">
+        <h1 className="text-2xl font-bold text-gray-800 mb-6 text-center">
+          🛒 Lista de Compras
+        </h1>
+
+        {/* Campo de entrada */}
+        <div className="flex gap-2">
+          <input type="text" placeholder="Digite um produto" className="flex-1 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+          <button onClick={CliqueiNoBotao} className="px-4 py-2 cursor-pointer bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
+            Adicionar
+          </button>
+        </div>
+
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
