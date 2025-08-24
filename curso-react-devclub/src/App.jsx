9b20ad602150cd91ef4e123e0544e1f054ace547
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-08-24 16:51:16 +0100

    Pegando a o valor do input via useRef

--- curso-react-devclub/src/App.jsx
+++ curso-react-devclub/src/App.jsx
@@ -3,9 +3,13 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  function CliqueiNoBotao(params) {
-    alert("Botão clicado");
+  const [produtos, setProdutos] = useState([]);
+  const inputRef = useRef();
+
+  function CliqueiNoBotao() {
+    setProdutos([inputRef.current.value, ...produtos]);
   }
+
   return (
     <div className=" flex items-center justify-center bg-gray-100 p-6">
       <div className="w-full max-w-md bg-white shadow-xl rounded-2xl p-6">
@@ -15,12 +19,18 @@ function App() {
 
         {/* Campo de entrada */}
         <div className="flex gap-2">
-          <input type="text" placeholder="Digite um produto" className="flex-1 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+          <input type="text" ref={inputRef} placeholder="Digite um produto" className="flex-1 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"/>
           <button onClick={CliqueiNoBotao} className="px-4 py-2 cursor-pointer bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
             Adicionar
           </button>
         </div>
 
+        {/* Lista de produtos */}
+        {
+          produtos.map(produto => {
+            return <div>{produto}</div>;
+          })
+        }
         
       </div>
     </div>
